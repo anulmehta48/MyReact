08a0cec1c98e8a64b10564cae8c54b904a6d5e90
anulmehta48 <anulmehta1848@gmail.com>
2023-02-19 18:52:37 +0530

    done

--- App.js
+++ App.js
@@ -1,3 +1,30 @@
+  import React from "react";
+  import ReactDOM from "react-dom/client"
+  
+
+  
+  /*
+  * PARCEL:-- ABOUT INFORMATIONS:-
+  * It create Server for our project
+  * HMR- Hot Module reloading
+  * File Watching Algorithms-c++
+  * BUILDING
+  * MINIFY
+  * Cleaning Code
+  * Dev and Production build
+  * Super fast build Algorithm
+  * Image optimaization also
+  * caching while development
+  * compressed 
+  * compatible with older version of browser
+  * HTTPS on dev
+  * Port number also manage this
+  * consitent hashing algorithms
+  * it is zero config 
+  * 
+  */
+  
+      
   //   const heading = React.createElement("h1", {}, "this is Original React");
   const heading = React.createElement("h1", {id:"id"}, "heading 1");
 
@@ -10,4 +37,4 @@
   const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //   root.render(heading);
-  root.render(container);
+  root.render(container); 
